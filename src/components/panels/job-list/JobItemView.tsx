030fcacbc01be78b8edaa0863574656cfe55a804
Frederick Bruch <65135023+freds-dev@users.noreply.github.com>
2026-09-17 09:35:37 +0200

    Update src/components/panels/job-list/JobItemView.tsx
    Co-authored-by: Norman Fomferra <norman.fomferra@brockmann-consult.de>

--- src/components/panels/job-list/JobItemView.tsx
+++ src/components/panels/job-list/JobItemView.tsx
@@ -76,7 +76,7 @@ export function JobItemView({
               </Text>
               <ActionIcon.Group style={revealStyle}>
                 {canUseRequest && (
-                  <Tooltip label="Use request">
+                  <Tooltip label="Edit process request">
                     <ActionIcon
                       {...styles.actionIcon.sm}
                       aria-label="Use request"
